1b40d83b1fa8477fc776d0775a28adb63d68d031
3k3n3 <80990247+3k3n3@users.noreply.github.com>
2023-04-27 14:33:15 +0100

    Added environmental configs

--- env.c
+++ env.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * printenv - Prints out all the environment variables
+ * print_env - prints environment variables
+ *
+ * Return: void
  */
-void printenv(void)
+void print_env(void)
 {
 	int i = 0;
 
@@ -14,18 +16,21 @@ void printenv(void)
 		i++;
 	}
 }
+
+
 /**
- * _unsetenv - deletes the variable name from the environment
- * @name: the name of the variable
+ * unset_env - deletes/unset variable name from env
+ * @name: variable name
  *
- * Return: 0 on success or -1 on error
+ * Return: 0 if success else -1
  */
-int _unsetenv(char *name)
+int unset_env(char *name)
 {
 	size_t len = 0;
 	int i = 0, flag = 0;
 
 	len = _strlen(name);
+
 	while (environ[i] != NULL)
 	{
 		if (strncmp(environ[i], name, len) == 0)
@@ -45,15 +50,16 @@ int _unsetenv(char *name)
 	return (-1);
 }
 
+
 /**
- * _setenv - changes or adds an environment variable
- * @name: the name of the variable
- * @value: the value of the variable
- * @overwrite: if the variable most be overwrited or not
+ * set_env - change or set new env variable
+ * @name: variable name
+ * @value: variable var
+ * @overwrite: if variable should be overwritten or not
  *
  * Return: 0 on success or -1 on error
  */
-int _setenv(char *name, char *value, int overwrite)
+int set_env(char *name, char *value, int overwrite)
 {
 	size_t len;
 	int i = 0;
@@ -67,49 +73,56 @@ int _setenv(char *name, char *value, int overwrite)
 			{
 				free(environ[i]);
 			}
-			environ[i] = create_variable(name, value);
+			environ[i] = create_var(name, value);
 			if (!environ[i])
 				return (-1);
 			return (0);
 		}
 		i++;
 	}
-	environ[i] = create_variable(name, value);
+	environ[i] = create_var(name, value);
 	if (!environ[i])
 		return (-1);
 	environ[i + 1] = NULL;
+
 	return (0);
 }
 
-/**
- * create_variable - creates a variable
- * @name: the name of the variable
- * @value: the value of the variable
- *
- * Return: the new variable
- */
-char *create_variable(char *name, char *value)
-{
-	char *var = NULL, *aux = NULL;
 
-	aux = str_concat(name, "=");
-	if (!aux)
+/**
+ * create_var - create variable
+ * @name: variable name
+ * @value: variable value
+ *
+ * Return: new variable
+ */
+char *create_var(char *name, char *value)
+{
+	char *var = NULL, *app = NULL;
+
+	app = str_concat(name, "=");
+	if (!app)
 		return (NULL);
-	var = str_concat(aux, value);
+	var = str_concat(app, value);
+
 	if (!var)
 		return (NULL);
-	free(aux);
+
+	free(app);
+
 	return (var);
 }
+
+
 /**
- * _getenv - Uses the environ variable in order to get a variable
- * @name: name of the variable
+ * get_env - get var from environ variable
+ * @name: variable name
  *
- * Return: Returns the value of the variable or NULL
+ * Return: value of the var or NULL
  */
-char *_getenv(const char *name)
+char *get_env(const char *name)
 {
-	char *token, *value, *cpy;
+	char *token, *val, *cpy;
 	size_t i = 0;
 
 	if (!name)
@@ -121,18 +134,18 @@ char *_getenv(const char *name)
 		if (_strcmp(name, token) == 0)
 		{
 			token = _strtok(NULL, '=');
-			value = malloc(sizeof(char) * _strlen(token) + 1);
-			if (!value)
+			val = malloc(sizeof(char) * _strlen(token) + 1);
+			if (!val)
 			{
 				free(cpy);
 				return (NULL);
 			}
-			_strcpy(value, token);
+			_strcpy(val, token);
 			free(cpy);
 			break;
 		}
 		i++;
 		free(cpy);
 	}
-	return (value);
+	return (val);
 }
